de10325f04373c2d5f9f9eb79f84d6bfca27cac1
Isaac Hernandez <isaac.ghc02@gmail.com>
2026-03-04 20:00:52 -0600

    INTT fail

--- fastNTT.cu
+++ fastNTT.cu
@@ -13,9 +13,6 @@
 // #define W		749
 #define MAX_TWIDDLES (N_DEF - 1)
 
-__constant__ uint64_t d_twiddles[MAX_TWIDDLES];
-__constant__ uint64_t d_twiddles_inv[MAX_TWIDDLES];
-
 uint64_t power(uint64_t base, uint64_t exp, uint32_t mod) {
   uint64_t res = 1;
   base %= mod;
@@ -28,63 +25,6 @@ uint64_t power(uint64_t base, uint64_t exp, uint32_t mod) {
   return res;
 }
 
-__global__ void fastNTT_simple(uint64_t *d_A, uint32_t N, uint32_t stage_offset,
-                               uint32_t len, uint32_t q) {
-  // *d_A -> Arreglo de entrada
-  // N -> Tamaño de d_A
-  // len -> Tamaño de un bloque lógico
-
-  int tid = blockDim.x * blockIdx.x + threadIdx.x;
-  if (tid >= N / 2)
-    return; // Salir si se lanzan mas de N/2 threads
-
-  uint32_t midlen = len / 2; // Número de butterflies por bloque logico
-
-  uint32_t block = tid / midlen; // A qué bloque lógico corresponde el thread
-  uint32_t k = tid % midlen;     // Índice del thread dentro del bloque lógico
-  uint32_t i = block * len;      // Índice base general del bloque lógico en A
-
-  // Lectura entradas Butterfly
-  uint64_t u = d_A[i + k];
-  uint64_t v = d_A[i + k + midlen];
-
-  uint64_t twiddle = d_twiddles[stage_offset + k];
-  v = (v * twiddle) % q;
-
-  uint64_t sum = (u + v) % q;
-  uint64_t diff = (u + q - v) % q;
-
-  d_A[i + k] = sum;
-  d_A[i + k + midlen] = diff;
-}
-
-__global__ void fastINTT_simple(uint64_t *d_A, uint32_t N,
-                                uint32_t stage_offset, uint32_t len,
-                                uint32_t q) {
-  int tid = blockDim.x * blockIdx.x + threadIdx.x;
-  if (tid >= N / 2)
-    return; // Salir si se lanzan mas de N/2 threads
-
-  uint32_t midlen = len / 2;
-
-  uint32_t block = tid / midlen;
-  uint32_t k = tid % midlen;
-  uint32_t i = block * len;
-
-  uint64_t u = d_A[i + k];
-  uint64_t v = d_A[i + k + midlen];
-
-  // Butterfly inversa
-  uint64_t sum = (u + v) % q;
-  uint64_t diff = (u + q - v) % q;
-
-  uint64_t twiddle = d_twiddles_inv[stage_offset + k];
-  diff = (diff * twiddle) % q;
-
-  d_A[i + k] = sum;
-  d_A[i + k + midlen] = diff;
-}
-
 void imprimir_arreglo(uint64_t *A, uint32_t N) {
   for (uint32_t i = 0; i < N; i++) {
     printf("%ld ", A[i]);
@@ -92,10 +32,8 @@ void imprimir_arreglo(uint64_t *A, uint32_t N) {
   printf("\n");
 }
 
-void precomputar_y_copiar_twiddles(uint32_t *stage_offsets) {
-
-  uint64_t twiddles[MAX_TWIDDLES];
-  uint64_t twiddles_inv[MAX_TWIDDLES];
+void precomputar_y_copiar_twiddles(uint32_t *stage_offsets, uint64_t *twiddles,
+                                   uint64_t *twiddles_inv) {
 
   uint32_t offset = 0;
   uint32_t stage = 0;
@@ -106,7 +44,6 @@ void precomputar_y_copiar_twiddles(uint32_t *stage_offsets) {
   for (int len = 2; len <= N_DEF; len <<= 1) {
 
     stage_offsets[stage] = offset;
-
     uint32_t mid = len >> 1;
 
     for (int k = 0; k < mid; k++) {
@@ -121,15 +58,111 @@ void precomputar_y_copiar_twiddles(uint32_t *stage_offsets) {
     offset += mid;
     stage++;
   }
+}
 
-  cudaMemcpyToSymbol(d_twiddles, twiddles, sizeof(uint64_t) * MAX_TWIDDLES);
+__global__ void ntt_kernel(uint64_t *d_A, const uint64_t *twiddles,
+                           const uint32_t *stage_offsets) {
 
-  cudaMemcpyToSymbol(d_twiddles_inv, twiddles_inv,
-                     sizeof(uint64_t) * MAX_TWIDDLES);
+  uint32_t tid = threadIdx.x;
+  uint32_t n = N_DEF;
+
+  uint32_t stage = 0;
+
+  for (uint32_t len = 1; len < n; len <<= 1) {
+
+    uint32_t butterflies = n >> 1;
+
+
+    for (uint32_t b = tid; b < butterflies; b += blockDim.x) {
+
+      // cada butterfly tiene:
+      uint32_t group =
+          b / len; // A qué bloque (grupo) lógico corresponde el thread
+      uint32_t j = b % len; // Índice del thread dentro del bloque lógico
+
+      uint32_t base =
+          group * (2 * len); // Índice base del bloque (grupo) lógico
+      uint32_t pos = base + j;
+
+      uint64_t u = d_A[pos];
+      uint64_t v = d_A[pos + len];
+
+      uint64_t w = twiddles[stage_offsets[stage] + j];
+
+      uint64_t t = (v * w) % Q;
+
+      uint64_t sum = u + t;
+      if (sum >= Q)
+        sum -= Q;
+
+      uint64_t diff = u + Q - t;
+      if (diff >= Q)
+        diff -= Q;
+
+      d_A[pos] = sum;
+      d_A[pos + len] = diff;
+    }
+
+    stage++;
+    __syncthreads();
+  }
+
+}
+
+__global__ void intt_kernel(uint64_t *d_A, const uint64_t *twiddles_inv,
+                            const uint32_t *stage_offsets,
+                            const uint32_t n_inv) {
+
+  const uint32_t tid = threadIdx.x;
+  const uint32_t n = N_DEF;
+  const uint32_t total_stages = __ffs(n) - 1; //log2(n)
+
+  uint32_t loop_stage = 0;
+
+  for (uint32_t len = n >> 1; len >= 1; len >>= 1, loop_stage++) {
+
+    uint32_t stage = total_stages - 1 - loop_stage;
+    uint32_t butterflies = n >> 1;
+
+    for (uint32_t b = tid; b < butterflies; b += blockDim.x) {
+
+      uint32_t group = b / len; // A qué bloque (grupo) lógico corresponde el thread
+      uint32_t j = b % len; // Índice del thread dentro del bloque lógico
+
+      uint32_t base = group * (2 * len); // Índice base del bloque (grupo) lógico
+      uint32_t pos = base + j;
+
+      uint64_t u = d_A[pos];
+      uint64_t v = d_A[pos + len];
+
+      uint64_t w = twiddles_inv[stage_offsets[stage] + j];
+
+      uint64_t t = (v * w) % Q;
+
+      uint64_t sum = u + t;
+      if (sum >= Q)
+        sum -= Q;
+
+      uint64_t diff = u + Q - t;
+      if (diff >= Q)
+        diff -= Q;
+
+      d_A[pos] = sum;
+      d_A[pos + len] = diff;
+    }
+
+    __syncthreads();
+  }
+
+  // Normalizar resultado por N^-1
+  for (uint32_t i = tid; i < n; i += blockDim.x)
+    d_A[i] = (d_A[i] * n_inv) % Q;
 }
 
 int main() {
   uint64_t A[N_DEF];
+
+  /*   ------------------ Init ----------------- */
 
   // Rellenar A con los datos de entrada
   for (uint32_t i = 0; i < N_DEF; i++)
@@ -138,44 +171,46 @@ int main() {
   // Imprimir entrada de ejemplo
   imprimir_arreglo(A, N_DEF);
 
-  // Pasar los datos a memoria global del GPU
-  uint64_t *d_A;
-  if (cudaMalloc((void **)&d_A, sizeof(uint64_t) * N_DEF) ==
-      cudaErrorMemoryAllocation)
-    return 1;
-  cudaMemcpy(d_A, A, sizeof(uint64_t) * N_DEF, cudaMemcpyHostToDevice);
-
   // Precomputar twiddles
-  uint8_t stages = 0;
+  uint64_t twiddles[MAX_TWIDDLES];
+  uint64_t twiddles_inv[MAX_TWIDDLES];
+  uint32_t stages = 0;
   uint32_t tmp = N_DEF;
   while (tmp > 1) {
-    tmp >>= 1;
     stages++;
+    tmp >>= 1;
   }
-
   uint32_t *stage_offsets = (uint32_t *)malloc(sizeof(uint32_t) * stages);
-  precomputar_y_copiar_twiddles(stage_offsets);
+  precomputar_y_copiar_twiddles(stage_offsets, twiddles, twiddles_inv);
 
-  // Parámetros para el kernel
-  uint32_t threads = N_DEF / 2;
+  // Pasar los datos a memoria global del GPU
+  uint64_t *d_A;
+  uint64_t *d_twiddles, *d_twiddles_inv;
+  uint32_t *d_stage_offsets;
+
+  cudaMalloc(&d_A, sizeof(uint64_t) * N_DEF);
+  cudaMalloc(&d_twiddles, sizeof(uint64_t) * MAX_TWIDDLES);
+  cudaMalloc(&d_twiddles_inv, sizeof(uint64_t) * MAX_TWIDDLES);
+  cudaMalloc(&d_stage_offsets, sizeof(uint32_t) * stages);
+
+  cudaMemcpy(d_A, A, sizeof(uint64_t) * N_DEF, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_twiddles, twiddles, sizeof(uint64_t) * MAX_TWIDDLES,
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_twiddles_inv, twiddles_inv, sizeof(uint64_t) * MAX_TWIDDLES,
+             cudaMemcpyHostToDevice);
+  cudaMemcpy(d_stage_offsets, stage_offsets, sizeof(uint32_t) * stages,
+             cudaMemcpyHostToDevice);
 
   // Medicion temporal
   cudaEvent_t start, stop;
   cudaEventCreate(&start);
   cudaEventCreate(&stop);
-
   cudaEventRecord(start);
 
+  /*   ------------------ NTT ----------------- */
+
   // Realizar NTT
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[0], 2, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[1], 4, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[2], 8, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[3], 16, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[4], 32, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[5], 64, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[6], 128, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[7], 256, Q);
-  fastNTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[8], 512, Q);
+  ntt_kernel<<<1, N_DEF / 2>>>(d_A, d_twiddles, d_stage_offsets);
 
   cudaEventRecord(stop);
   cudaEventSynchronize(stop);
@@ -186,31 +221,22 @@ int main() {
   // Imprimir resultado de la NTT
   imprimir_arreglo(A, N_DEF);
 
+  /*   ------------------ INTT ----------------- */
+
   // Realizar INTT
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[8], 512, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[7], 256, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[6], 128, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[5], 64, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[4], 32, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[3], 16, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[2], 8, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[1], 4, Q);
-  fastINTT_simple<<<1, threads>>>(d_A, N_DEF, stage_offsets[0], 2, Q);
+  uint32_t N_inv = power(N_DEF, Q - 2, Q);
+  intt_kernel<<<1, N_DEF / 2>>>(d_A, d_twiddles_inv, d_stage_offsets, N_inv);
 
   // Copiar los datos de salida del GPU al Host
   cudaMemcpy(A, d_A, sizeof(uint64_t) * N_DEF, cudaMemcpyDeviceToHost);
 
-  // Escalar resultado por N^-1
-  uint32_t N_inv = power(N_DEF, Q - 2, Q);
-  for (int i = 0; i < N_DEF; i++)
-    A[i] = (A[i] * N_inv) % Q;
-
   // Imprimir resultado de la INTT
   imprimir_arreglo(A, N_DEF);
 
+  /*   ------------------ Mediciones ----------------- */
+
   float ms = 0.0f;
   cudaEventElapsedTime(&ms, start, stop);
-
   printf("La NTT tardó: %f ms\n", ms);
 
   cudaFree(d_A);
